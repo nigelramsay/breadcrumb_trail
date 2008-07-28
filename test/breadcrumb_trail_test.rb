ba7938ef01cd433e84c4faf60a328703f2ec6e02
Nigel Ramsay <nigelramsay@nigel-ramsays-macbook.local>
2008-07-28 19:57:59 +1200

    dummy test

--- test/breadcrumb_trail_test.rb
+++ test/breadcrumb_trail_test.rb
@@ -3,6 +3,6 @@ require 'test/unit'
 class BreadcrumbTrailTest < Test::Unit::TestCase
   # Replace this with your real tests.
   def test_this_plugin
-    flunk
+    assert(true, "Replace with actual test")
   end
 end
