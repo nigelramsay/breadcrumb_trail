8945b53a1c80ada927872aac36ea93e0b5d918fd
masone <ema@rh-productions.ch>
2008-11-17 21:58:01 +0100

    Escaping label in helper

--- lib/breadcrumb_trail_helper.rb
+++ lib/breadcrumb_trail_helper.rb
@@ -4,8 +4,8 @@ module BreadcrumbTrailHelper
       list_items = String.new
       
       @crumbs.each do |c|
-        list_items << content_tag(:li, link_to(c[:label], c[:link]), :class => c[:class]) if c[:link]
-        list_items << content_tag(:li, c[:label], :class => c[:class]) unless c[:link]
+        list_items << content_tag(:li, link_to(h(c[:label]), c[:link]), :class => c[:class]) if c[:link]
+        list_items << content_tag(:li, h(c[:label]), :class => c[:class]) unless c[:link]
       end
       
       content_tag :ol, list_items, :class => 'breadcrumb'
