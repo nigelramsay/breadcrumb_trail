4995fc9cc3abf99f455e87560733f860592614f9
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2008-11-18 16:56:20 +1300

    allow declaration of a root crumb

--- lib/breadcrumb_trail.rb
+++ lib/breadcrumb_trail.rb
@@ -1,6 +1,10 @@
 module BreadcrumbTrail
+  def add_root_crumb(crumb={})
+    defaults = {:link => root_path, :label => 'Home', :class => 'first'}
+    @crumbs = [defaults.merge(crumb)]
+  end
   def add_crumb(crumb)
-    @crumbs ||= [:link => root_path, :label => 'Home', :class => 'first']
+    add_root_crumb unless @crumbs
     @crumbs << crumb
   end
 end
